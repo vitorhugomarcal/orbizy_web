ad457bf89616830d089a439f2151d9dad60c5b11
vitorhugomarcal <vitorhugomarcal@gmail.com>
2025-01-26 17:36:40 -0300

    api change 4

--- kubb.config.ts
+++ kubb.config.ts
@@ -35,6 +35,7 @@ export default defineConfig(() => {
       }),
       pluginClient({
         baseURL: "https://api.orbizy.app",
+        client: "axios",
       }),
     ],
   }
--- src/http/generated/clients/getClientsMonth.ts
+++ src/http/generated/clients/getClientsMonth.ts
@@ -1,13 +1,6 @@
-import type {
-  RequestConfig,
-  ResponseErrorConfig,
-} from "@kubb/plugin-client/clients/axios"
-import client from "@kubb/plugin-client/clients/axios"
-import type {
-  GetClientsMonth401,
-  GetClientsMonth404,
-  GetClientsMonthQueryResponse,
-} from "../models/ClientsController/GetClientsMonth.ts"
+import client from '@kubb/plugin-client/clients/axios'
+import type { GetClientsMonthQueryResponse, GetClientsMonth401, GetClientsMonth404 } from '../models/ClientsController/GetClientsMonth.ts'
+import type { RequestConfig, ResponseErrorConfig } from '@kubb/plugin-client/clients/axios'
 
 export function getGetClientsMonthUrl() {
   return `https://api.orbizy.app/clients/month` as const
@@ -17,17 +10,11 @@ export function getGetClientsMonthUrl() {
  * @description Retrieve client count for the current month
  * {@link /clients/month}
  */
-export async function getClientsMonth(
-  config: Partial<RequestConfig> & { client?: typeof client } = {}
-) {
+export async function getClientsMonth(config: Partial<RequestConfig> & { client?: typeof client } = {}) {
   const { client: request = client, ...requestConfig } = config
 
-  const res = await request<
-    GetClientsMonthQueryResponse,
-    ResponseErrorConfig<GetClientsMonth401 | GetClientsMonth404>,
-    unknown
-  >({
-    method: "GET",
+  const res = await request<GetClientsMonthQueryResponse, ResponseErrorConfig<GetClientsMonth401 | GetClientsMonth404>, unknown>({
+    method: 'GET',
     url: getGetClientsMonthUrl().toString(),
     ...requestConfig,
   })
