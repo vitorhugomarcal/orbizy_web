6bb12af52cd3b7ae19e22d1d8834e13f78c53d13
vitorhugomarcal <vitorhugomarcal@gmail.com>
2025-01-26 21:07:54 -0300

    api change 7

--- src/pages/app/dashboard/month-estimates-card.tsx
+++ src/pages/app/dashboard/month-estimates-card.tsx
@@ -7,7 +7,7 @@ import { ReceiptText } from "lucide-react"
 export function MonthEstimatesCard() {
   const { data, isLoading } = useGetEstimateMonth()
 
-  console.log(data)
+  console.log("MONTH ESTIMATE=> ", data)
 
   return (
     <Card>
@@ -18,7 +18,7 @@ export function MonthEstimatesCard() {
         <ReceiptText className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        {isLoading || !data ? (
+        {isLoading ? (
           <div className="space-y-1">
             <Skeleton className="h-8 w-32" />
             <Skeleton className="h-4 w-full" />
@@ -26,18 +26,18 @@ export function MonthEstimatesCard() {
         ) : (
           <>
             <span className="text-2xl font-bold tracking-tight">
-              {data.total}
+              {data?.total}
             </span>
             <p className="text-xs text-muted-foreground">
               <span
                 className={`${
-                  data.percentageChange >= 0
+                  data?.percentageChange! >= 0
                     ? "text-emerald-500 dark:text-emerald-400"
                     : "text-red-500 dark:text-red-400"
                 }`}
               >
-                {data.percentageChange >= 0 ? "+" : ""}
-                {data.percentageChange.toFixed(1)}%
+                {data?.percentageChange! >= 0 ? "+" : ""}
+                {data?.percentageChange.toFixed(1)}%
               </span>{" "}
               em relação ao mês passado
             </p>
