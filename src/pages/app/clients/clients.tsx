43cbeaae579f1850b0a0932355db2f726a31bbd1
vitorhugomarcal <vitorhugomarcal@gmail.com>
2025-01-27 14:24:45 -0300

    api clients

--- src/pages/app/clients/clients.tsx
+++ src/pages/app/clients/clients.tsx
@@ -7,31 +7,34 @@ import {
 import { Separator } from "@/components/ui/separator"
 import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar"
 import { Helmet } from "react-helmet-async"
-// import { ClientsTable } from "./clients-table"
+// import { SupplierTable } from "./supplier-table"
 
-import { ClientRegister } from "./client-register"
-
-export function Clients() {
+export function Supplier() {
   return (
     <SidebarInset>
       <Helmet title="Clientes" />
-      <header className="flex h-16 shrink-0 items-center gap-2 lg:justify-between mr-4">
+      <header className="flex h-16 shrink-0 items-center gap-2 justify-between mr-4">
         <div className="flex items-center gap-2 px-4">
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="mr-2 h-4" />
           <Breadcrumb>
             <BreadcrumbList>
+              {/* <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem> */}
               <BreadcrumbItem className="hidden md:block">
                 <BreadcrumbPage>Clientes</BreadcrumbPage>
               </BreadcrumbItem>
             </BreadcrumbList>
           </Breadcrumb>
         </div>
-
-        <ClientRegister />
       </header>
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        {/* <ClientsTable /> */}
+        {/* <SupplierTable /> */}
       </div>
     </SidebarInset>
   )
