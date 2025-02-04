d2ada90a9f43f881b7229486360f97c58979be78
vitorhugomarcal <vitorhugomarcal@gmail.com>
2025-02-04 17:39:35 -0300

    api clients

--- src/env.ts
+++ src/env.ts
@@ -5,12 +5,4 @@ const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "production", "test"]).default("dev"),
 })
 
-const _env = envSchema.safeParse(process.env)
-
-if (_env.success === false) {
-  console.error("❌ Invalid environment variables", _env.error.format())
-
-  throw new Error("Invalid environment variables")
-}
-
-export const env = _env.data
+export const env = envSchema.parse(import.meta.env)
